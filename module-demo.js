cb7e6f9b35d03f1517c23e7b85f410b193b20bd1
Aman Sachdev <aman.sachdev@cleardata.com>
2022-08-13 18:50:03 +0530

    make it better

---
+++ module-demo.js
@@ -0,0 +1,20 @@
+// var myModule = require('./my-module.js')
+
+// console.log(myModule.myText);
+
+// var _ = require('lodash')
+
+// console.log(_.random(10,1000000))
+
+// var fs = require('fs')
+
+// fs.readFile('./data.json', 'utf-8', (err, data) => {
+//     var data = JSON.parse(data)
+//     console.log(data)
+// });
+
+// var fs = require('fs')
+
+// fs.readdir('c:/', (err,data) => {
+//     console.log(data)
+// })
---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+var messages = [
+    {name: "Aman",message:'hi'},
+    {name: "Time",message:'hi'},
+]
+
+app.get('/messages', (req,res) => {
+    res.send(messages)
+})
+
+app.post('/messages', (req,res) => {
+    res.sendStatus(200)
+    messages.push(req.body)
+    io.emit("message", req.body)
+})
+
+io.on('connection',(socket)=>{
+    console.log("User connected")
+})
+
+var server = http.listen(3000, ()=>{
+    console.log("Server is listening on port", server.address().port)
+})
